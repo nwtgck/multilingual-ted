d81a892373d206d3e1791e6af84ca9eb74cf543e
Ryo Ota <nwtgck@gmail.com>
2014-10-18 10:24:44 +0900

    - 文章字幕作成

--- public_html/angular/controllers.js
+++ public_html/angular/controllers.js
@@ -76,33 +76,121 @@ ANG_TED_APP.
 		var talk_id = $location.search()['talk_id'];
 		$scope.talk_id = talk_id;
 		$scope.talk;
+		/*
+			.mp4ファイルの動画URL
+		*/
 		$scope.video_url = "";
+		/*
+			動画の再生位置
+		*/
 		$scope.video_time = 0;
-		$scope.subtitleLanguages = [];
+		/*
+			talkの各言語の字幕データ
+		*/
 		$scope.subtitles = {};
+		/*
+			リアルタイムに表示している字幕
+			構造
+			{"ja": " 今の日本語字幕", "en": "real time English subtitle"}
+		*/
 		$scope.nowSubtitles = {};
+		/*
+			リアルタイムに表示する、文章形式の字幕
+			構造
+			{"en": ["This is", "my first trip"]}
+		*/
+		$scope.nowSentenseSubtitles = {};
 
-		var setSubtitle = function(sec){
-			var subtitleLang;
-			for(var lang in $scope.nowSubtitles){
-				subtitleLang = lang;
-				break;
+		/*
+			 センテンスの終わり
+		*/
+		var sentense_end_indexs = [];
+		/*
+			文の区切れ目を調べる
+		*/
+		var setSentenseEnds = function(){
+			var en_subtitles = $scope.subtitles["en"];
+			var i, j;
+			for(i = 0; i < en_subtitles.length; i++){
+				var sub = en_subtitles[i].caption.content;
+				// TODO: 正規表現で文末判定に
+				var end = sub.slice(-1);
+				if(end == "." || end == ")" || end == "?" || end == "!"){
+					sentense_end_indexs.push(i);
+				}
+
 			}
+
+			// test
+			var buff;
+			for(i = 0; i < sentense_end_indexs.length; i++){
+				buff = "";
+				for(j = sentense_end_indexs[i-1]+1 || 0; j <= sentense_end_indexs[i]; j++){
+					buff += " " + $scope.subtitles["en"][j].caption.content;
+				}
+				console.log(buff);
+			}
+			console.log(sentense_end_indexs);
+		};
+
+
+		/*
+			再生位置から、いまの字幕のインデックスを取得
+		*/
+		var getSubtitleIndex = function(sec){
+			var subtitleLang = Object.keys($scope.nowSubtitles)[0];
+
 			var subtitles = $scope.subtitles[subtitleLang];
 			for(var i = 0; i < subtitles.length; i++){
 				var subtitle = subtitles[i].caption;
 				var startTime = subtitle.startTime;
 				if(startTime > sec*1000){
-					break;
+					return i-1;
 				}
 			}
-			if(i == 0) return;
+		};
+
+		/*
+			再生位置から、字幕を設定
+		*/
+		var setSubtitle = function(sec){
+			var idx = getSubtitleIndex(sec);
+			if(idx == -1) return;
 
 			// iを元に字幕を入れる
 			for(var lang in $scope.nowSubtitles){
-				$scope.nowSubtitles[lang] = $scope.subtitles[lang][i-1].caption.content;
+				$scope.nowSubtitles[lang] = $scope.subtitles[lang][idx].caption.content;
+			}
+		};
+
+		/*
+			再生位置から、文章字幕を設定
+		*/
+		var setSentenseSubtitles = function(sec){
+			var idx = getSubtitleIndex(sec);
+			var start_idx = 0;
+			var end_idx = 0;
+			if(idx == -1) return;
+
+			// 文章のスタートとエンドを探す
+			for(var i = 0; i < sentense_end_indexs.length; i++){
+				if(idx <= sentense_end_indexs[i]){
+					end_idx = sentense_end_indexs[i];
+					break;
+				}
+				start_idx = sentense_end_indexs[i]+1;
 			}
 			
+			// 字幕をセット
+			for(var lang_code in $scope.subtitles){
+				$scope.nowSentenseSubtitles[lang_code] = [];
+				for(i = start_idx; i <= end_idx; i++){
+					$scope.nowSentenseSubtitles[lang_code].push({
+						now: (idx == i)? true: false,
+						content: " "+$scope.subtitles[lang_code][i].caption.content
+					});
+				}
+			}
 		};
 
 		// 字幕を表示するか決める
@@ -124,10 +212,15 @@ ANG_TED_APP.
 			gettingSubtitle(function(subtitles){
 				$scope.subtitles[lang] = subtitles;
 				$scope.nowSubtitles[lang] = subtitleText;
+
+				//　英語字幕の時は文章の区切れ目を調べる
+				if(lang == "en"){
+					setSentenseEnds();
+				}
 			}, talk_id, lang);
 		};
 
-		// 字幕を表示しない
+		// 字幕非表示にする
 		$scope.clearAll = function(){
 			$scope.nowSubtitles = {};
 		};
@@ -147,6 +240,7 @@ ANG_TED_APP.
 				$scope.$apply(function(){
 					$scope.video_time = myVideo.currentTime;
 					setSubtitle($scope.video_time);
+					setSentenseSubtitles($scope.video_time);
 				});	
 			}, false);
 
